4d3ececb38495dd07123765a362e2745fe8fe8b8
jay <ronokamau@gmail.com>
2023-03-17 00:25:10 +0300

    javascript must be nice

---
+++ app.js
@@ -0,0 +1,4 @@
+var life = 100;
+var name = 'Jay'; //that is not my name (:
+var doit = document.getElementById('demo').innerHTML = 'Vs what I need to learn';
+console.log(name, ", Welcome sir, It has been" , life, "years!")
