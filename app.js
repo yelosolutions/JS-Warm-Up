ad21dd472b8a0c2bcab787d500a654c1a7d6617d
jay <ronokamau@gmail.com>
2023-03-18 23:44:29 +0300

    Up to the sidebar

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var life = 100;
-var name = 'Jay'; //that is not my name (:
-var doit = document.getElementById('demo').innerHTML = 'Vs what I need to learn';
-console.log(name, ", Welcome sir, It has been" , life, "years!")
+const model = ['android', 'AI', 'Neo-AI'];
+
+function createRobot () {
+    let robotName = 'R2D2';
+    let robotAge = 30;
+    console.log("Initializing", robotName, "... I was first programmed ", robotAge, "years ago." )
+    console.log(name, ", Welcome sir, It has been" , robotAge, "years! Today\'s date is 7/8/2040")
+    if (robotAge <= 10 ) {
+        console.log("I am the new model, you know ", model[2])
+    } else if(robotAge <= 20){
+        console.log("When we were first created, humans thought we were going to replace them, we are ", model[1])
+    }
+    
+    else {
+        console.log("My name is ", robotName, "and I am an ", model[0])
+    };
+};
+createRobot();
+
+const toUpper = (text) => {
+    const upperCase = text.toUpperCase();
+    console.log(upperCase)
+}
+toUpper('kamau')
+
+const sayHEllo = () => {
+    console.log(name, "says, Hello!")
+}
+sayHEllo();
+
