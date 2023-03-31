bc2add8f087f9edf70b3f138a01764de85b6de1f
jay <ronokamau@gmail.com>
2023-03-31 14:51:13 +0300

    restructuring

---
+++ JFK/drawcats.js
@@ -0,0 +1,6 @@
+var drawCats = function(number_of_cats) {
+    for (i = 0; i < number_of_cats; i++) {
+        console.log(i + " =^.^=");
+    }
+};
+drawCats(6)
--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 
+//the && boolean operation(AND)
+var studentName = "Jay";
+var hasApples = true;
+var hasOranges = false;
+if (hasApples && hasOranges === true) {
+    console.log("The schools diet plan requires atleast one fruit")
+}
+else {
+    console.log("Lucky you ", studentName, ", atleast you have one fruit")
+};
+
+//the || boolean operation(OR)
+var mathPass = false;
+var englishPass = false;
+if (englishPass || mathPass == true){
+    var pass = true;
+    if (pass === true){
+        console.log("Congratulations", studentName, "! You have passed");
+    }        
+}
+else {
+    var fail = !pass;
+    if( fail === !pass){
+        console.log("Too bad!")
+    }
+}
