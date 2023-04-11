560e4e0468e51bab2b94ce1ef2d76e8074b36d97
jay <ronokamau@gmail.com>
2023-04-12 02:32:57 +0300

    hangman game using functions

--- JFK/hangman1.js
+++ JFK/hangman1.js
@@ -8,7 +8,59 @@ var pickWord = () => {
         "pilot"
     ];
     return words[Math.floor(Math.random() * words.length)];
+};
+var word = pickWord();
+
+var setupAnswerArray = (word) => {
+    var array = []
+    for (var i = 0; i < word.length; i++){
+        array[i] = "_";
+    }
+    return array;
+
 }
 
-var word = pickWord();
-console.log(word)
+var answerArray = setupAnswerArray(word);
+
+var remainingLetters = word.length;
+
+
+var showPlayerProgress = (answerArray) => {
+    alert("The HANGMAN game: Guess the letters of the following word: " + answerArray);   
+};
+
+var getGuess = () => {
+    return prompt("Play game or click cancel to quit");
+};
+
+var updateGameState = (guess, word, answerArray) =>{
+    for (var j = 0; j < word.length; j++){
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            return j;
+        }
+    }
+           
+}
+
+var showAnswerAndCongratulatePlayer = (answerArray) => {
+    alert(answerArray.join(" "))
+    alert("Congratulations! The word was " + word)
+}
+
+
+while (remainingLetters > 0) {
+    showPlayerProgress(answerArray);
+    var guess = getGuess();
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert("Please enter a single letter.");
+    } else {
+        var correctGuesses = updateGameState(guess, word, answerArray);
+        remainingLetters -= correctGuesses;
+    }
+}
+
+
+showAnswerAndCongratulatePlayer(answerArray);
