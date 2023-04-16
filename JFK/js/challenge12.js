29e7d02a0836b5358517c5266516fdec73a52be3
jay <ronokamau@gmail.com>
2023-04-16 23:07:11 +0300

    the heading to follow clicks challenge

--- JFK/js/challenge12.js
+++ JFK/js/challenge12.js
@@ -5,7 +5,9 @@ Whenever you click the page, the heading should move to the
 click location.
 */
 
+//allows the mouse to be clicked anywhere in the html page
 $("html").click( function (event) {
+    //makes the heading element with given id to move where mouse is clicked
     $("#heading").offset({
         left: event.pageX,
         top: event.pageY
