32649ff48592c9fd68cb04e4e983588c012a8a8e
jay <ronokamau@gmail.com>
2023-04-23 01:06:58 +0300

    A SNOWMAN-DRAWING FUNCTION

--- JFK/js/canvas.js
+++ JFK/js/canvas.js
@@ -10,7 +10,7 @@ var ctx = canvas.getContext('2d');
 drawing context.
 */
 ctx.fillStyle = "green";
-var colorScheme = ["GoldenRod", "Peru", "Cyan"]
+var colorScheme = ["GoldenRod", "Peru", "Cyan", "Blue", "Yellow", "Red"]
 
 /*
 for(var i = 0; i < colorScheme.length; i++){
@@ -31,7 +31,7 @@ for(var i = 0; i < colorScheme.length; i++){
     ctx.strokeRect(i * 50 , 0, 50, 90);
 };
 
-
+// drawing a little robot using the fillRect method.
 ctx.fillStyle = "grey";
 
 var square = ctx.fillRect(100, 100, 50, 50);
@@ -47,6 +47,12 @@ ctx.lineWidth = 4;
 var square = ctx.strokeRect(290, 130, 20, 20);
 
 
+
+/**
+ * Try drawing this happy stickman using the beginPath, moveTo, lineTo, 
+ * and stroke methods. You can use the strokeRect method for the head. 
+ * The head is a 20-pixel-by-20-pixel square, and the line width is 4 pixels.
+ */
 ctx.beginPath();
 ctx.moveTo(300, 150);
 ctx.lineTo(300, 210);
@@ -64,11 +70,96 @@ ctx.moveTo(300, 210);
 ctx.lineTo(360, 280);
 ctx.stroke();
 
+
+/**
+ * To fill a closed path with color instead of just drawing an outline, 
+ * you can use the fill method instead of stroke. 
+ * Example: a simple grey house
+ */
 ctx.beginPath();
-ctx.moveTo(300, 10);
-ctx.lineTo(240, 70);
-ctx.lineTo(240, 130);
-ctx.lineTo(360, 130);
-ctx.lineTo(360, 70);
-ctx.lineTo(300, 10);
+ctx.moveTo(300, 5);
+ctx.lineTo(240, 75);
+ctx.lineTo(240, 125);
+ctx.lineTo(360, 125);
+ctx.lineTo(360, 75);
+ctx.lineTo(300, 5);
 ctx.fill();
+
+/**To draw a circle, you set the circle’s center coordinates and radius 
+and tell JavaScript how much of the circle to draw by providing a starting 
+angle and ending angle as arguments.
+We pass false for the final argument, which tells arc to draw in a clockwise direction. 
+*/
+ctx.beginPath()
+ctx.arc(400, 100, 20, 0, Math.PI * 2, false);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(400, 160, 20, 0, Math.PI, false);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(400, 220, 20, 0, Math.PI * 3/2, false);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(400, 280, 20, 0, Math.PI / 2, false);
+ctx.stroke();
+
+/**
+ * a function to draw circles that lets us ignore (PI and direction) so 
+ * that we have to supply only the x, y, and radius arguments. 
+ */
+var drawCircle = function (x, y, radius){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.stroke();
+}
+
+/**How would you modify our circle function to make it fill the 
+circle instead of outline it? Add a fourth argument, a Boolean, that says
+whether the circle should be filled or outlined. Passing true indicates 
+that you want the circle to be filled. You can call the argument fillCircle.
+Using your modified function, draw this snowman, using a mix of outlined 
+and filled circles
+*/
+var circle = function (x, y, radius, filled){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    if(filled){
+        ctx.fill();
+    } else{
+        ctx.stroke();
+    }
+}
+
+
+//draw a snowman, using a mix of outlined and filled circles
+ctx.strokeStyle = "black";
+ctx.fillStyle = "black";
+circle(500, 300, 30, false);
+circle(490, 295, 3, true);
+circle(510, 295, 3, true);
+
+circle(500, 372, 40, false);
+circle(500, 370, 3, true);
+circle(500, 380, 3, true);
+circle(500, 390, 3, true);
+
+ctx.fillStyle = "goldenrod";
+circle(500, 305, 3, true);
+
+
+
+
+//draws concentric circles with varying radii
+var randomColor = Math.floor(Math.random() * colorScheme.length);
+var radius = 10
+for(var i = 0; i < colorScheme.length; i++){
+    var randomColor = Math.floor(Math.random() * colorScheme.length);
+    ctx.strokeStyle = colorScheme[randomColor];
+    drawCircle(500, 100, radius);
+    radius += 10;
+};
+
+
--- JFK/js/trycanvas.js
+++ JFK/js/trycanvas.js
@@ -1,10 +1,82 @@
 var canvas = document.getElementById("canvas");
-var drawingContext = canvas.getContext("2d");
+var drawCxt = canvas.getContext("2d");
+
+/**
+ * implement a function that draws a snowman at a specified location, 
+ * so that calling this => drawSnowman(50, 50); would draw a snowman 
+ * at the point (50, 50).
+*/
+var circle = function (x, y, radius, filled){
+    drawCxt.beginPath();
+    drawCxt.arc(x, y, radius, 0, Math.PI * 2, false);
+    if(filled){
+        drawCxt.fill();
+    } else{
+        drawCxt.stroke();
+    }
+}
+
+var drawSnowman = function (w, z) {
+    drawCxt.lineWidth = 4;
+    drawCxt.strokeStyle = "black";
+    drawCxt.fillStyle = "black";
+    circle(w, z, 30, false);
+    circle(w - 10, z-5, 3, true);
+    circle(w + 10, z-5, 3, true);
+
+    circle(w, z + 74, 40, false);
+    circle(w, z + 55, 3, true);
+    circle(w, z + 75, 3, true);
+    circle(w, z + 95, 3, true);
+
+    drawCxt.fillStyle = "goldenrod";
+    circle(w, z + 5, 3, true);
+};
+
+drawSnowman(200, 200);
+
+/**
+ * Write a function that will take an array of points like this:
+var points = [[50, 50], [50, 100], [100, 100], [100, 50],
+[50, 50]];
+drawPoints(points);
+and draw a line connecting the points. In this example, the function would 
+draw a line from (50, 50) to (50, 100) to (100, 100) to (100, 50) and back 
+to (50, 50). Now use this function to draw the following points:
+var mysteryPoints = [[50, 50], [50, 100], [25, 120],
+[100, 50], [70, 90], [100, 90], [70, 120]];
+drawPoints(mysteryPoints);
+Hint: You can use points[0][0] to get the first x-coordinate and points[0][1] 
+to get the first ycoordinate.
+*/
+
+var points = [[50, 50], [50, 100], [100, 100], [100, 50],
+[50, 50]];
+var mysteryPoints = [[50, 50], [50, 100], [25, 120],[100, 50], [70, 90], [100, 90], [70, 120]];
 
 
-var square = drawingContext.fillRect(100, 100, 50, 50);
-var rectangle = drawingContext.fillRect(120, 150, 10, 30);
-var rectangle2 = drawingContext.fillRect(50, 180, 150, 10);
-var square = drawingContext.fillRect(90, 190, 70, 70);
-var rectangle3 = drawingContext.fillRect(90, 260, 10, 50);
-var rectangle4 = drawingContext.fillRect(150, 260, 10, 50);
+var drawPoints = function (points) {
+    drawCxt.lineWidth = 4;
+    drawCxt.strokeStyle = "black";
+    for (var i=0; i < points.length; i++){
+        drawCxt.beginPath();
+        drawCxt.moveTo(points[i][0], points[i][1] );
+        drawCxt.lineTo(points[i+1][0], points[i+1][1]);
+        drawCxt.stroke();
+    }
+}
+
+drawPoints(mysteryPoints);
+
+/*
+drawCxt.beginPath();
+drawCxt.moveTo(50, 50);
+drawCxt.lineTo(50, 100);
+drawCxt.lineTo(25, 120);
+drawCxt.lineTo(100, 50);
+drawCxt.lineTo(70, 90);
+drawCxt.lineTo(100, 90);
+drawCxt.lineTo(70, 120);
+drawCxt.stroke();
+*/
+
